4ef6e8d2793817a8faa26a0c086859094ebe96c6
Dirga36 <fuaddabbad@gmail.com>
2025-10-03 15:25:10 +0700

    fix callback URI

--- app/api/login/route.ts
+++ app/api/login/route.ts
@@ -1,7 +1,7 @@
 import { NextResponse } from "next/server";
 
 const CLIENT_ID = process.env.ORCID_CLIENT_ID!;
-const REDIRECT_URI = "http://localhost:3000/api/callback";
+const REDIRECT_URI = "https://nextjs-boilerplate-one-orcin-37.vercel.app/api/callback";
 
 export async function GET() {
   const authUrl = `https://orcid.org/oauth/authorize?client_id=${CLIENT_ID}&response_type=code&scope=/authenticate&redirect_uri=${REDIRECT_URI}`;
